5dbb85798db558f47aebe10f3d63bcd512077e34
Easily JS <easilyjs@gmail.com>
2022-04-11 20:15:36 +0800

    patch: update playground url & set click to run

--- constants/playground.ts
+++ constants/playground.ts
@@ -1,2 +1,2 @@
 export const PLAYGROUND_URL =
-  'https://stackblitz.com/edit/react-ts-xbnlex?embed=1&file=App.tsx&hideExplorer=1&hideNavigation=1';
+  'https://stackblitz.com/edit/react-atom?ctl=1&embed=1&file=App.tsx&hideExplorer=1&hideNavigation=1';
